d089ae94cedcf2a727ba07aea4a9a75f6082b08d
jcnpereira <casimiro@ipt.pt>
2026-03-09 12:26:39 +0000

    configuration of Category model

--- API/API/Models/Category.cs
+++ API/API/Models/Category.cs
@@ -1,4 +1,7 @@
-﻿namespace API.Models {
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace API.Models {
 
 
    /// <summary>
@@ -14,7 +17,10 @@
       /// <summary>
       /// the name of the Category
       /// </summary>
-      public string Name { get; set; }
+      [StringLength(50)] // the max length of the text
+      [Required(ErrorMessage ="o nome da categoria é obrigatório")] // error message if you do not write anything
+      [Display(Name="Nome da Categoria")]  // the name to show on screen
+      public string Name { get; set; } = "";
 
       /*  ************************************** 
       *  Relationships
@@ -23,7 +29,7 @@
       /// <summary>
       /// List of photos that a category has
       /// </summary>
-      public ICollection<Photography>ListOfPhotos { get; set; }
+      public ICollection<Photography> ListOfPhotos { get; set; } = [];
 
    }
 }
