73d6196d8c3ae8d98c6472443d6ce9a56f0ea2a0
jcnpereira <casimiro@ipt.pt>
2026-03-23 12:23:22 +0000

    add of swagger tool

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -3,6 +3,9 @@ using API.Data.Seed;
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,31 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+
+
+// add Swagger tool to document the API
+// Add the Swagger package
+// <PackageReference Include="Swashbuckle.AspNetCore" Version="8.1.4" />  // for .NET Framework 8
+// <PackageReference Include="Swashbuckle.AspNetCore" Version="10.1.5" />   // for .NET Framework 10
+builder.Services.AddSwaggerGen(c => {
+   c.SwaggerDoc("v1", new OpenApiInfo {
+      Title = "Minha API de gestão de Fotos",
+      Version = "v1",
+      Description = "API para gestão de categorias, fotografias e utilizadores"
+   });
+   /*
+     // Caminho para o XML gerado
+       var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+       var xmlPath = Path.Combine(AppContext.BaseDirectory,xmlFile);
+       c.IncludeXmlComments(xmlPath);
+   */
+});
+
+
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +52,10 @@ if(app.Environment.IsDevelopment()) {
 
    // use the seed methods
    app.UseItToSeedSqlServer();
+
+   // start the 'middleware' Swagger
+   app.UseSwagger();
+   app.UseSwaggerUI();
 
 }
 else {
